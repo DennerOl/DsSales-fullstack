34da3232476b0c948140f9b46a03fe29fe62e323
DennerOl <dener155@hotmail.com>
2024-05-22 21:34:52 -0300

    inicio do grafico de pizza

--- dssales-frontend/src/App.tsx
+++ dssales-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Filter from './components/filter';
 import Header from './components/header';
+import PierChartCard from './components/pie-chart-card';
 import SalesByDate from './components/sales-by-date';
 import SalesSummary from './components/sales-summary';
 
@@ -13,6 +14,8 @@ function App() {
         <SalesByDate />
         <div className="sales-overview-container">
           <SalesSummary />
+          <PierChartCard name="Lojas" labels={['Uberlandia', 'Araguari']} series={[40, 60]} />
+          <PierChartCard name="Pagamento" labels={['Uberlandia', 'Resende']} series={[50, 50]} />
         </div>
       </div>
     </>
