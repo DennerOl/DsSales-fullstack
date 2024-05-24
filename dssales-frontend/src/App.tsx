52efc83cb88b609b399252e73f9f70110c41f277
DennerOl <dener155@hotmail.com>
2024-05-23 21:02:53 -0300

    Criada a tabela

--- dssales-frontend/src/App.tsx
+++ dssales-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/header';
 import PierChartCard from './components/pie-chart-card';
 import SalesByDate from './components/sales-by-date';
 import SalesSummary from './components/sales-summary';
+import SalesTable from './components/sales-table';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <PierChartCard name="Lojas" labels={['Uberlandia', 'Araguari']} series={[40, 60]} />
           <PierChartCard name="Pagamento" labels={['Uberlandia', 'Resende']} series={[50, 50]} />
         </div>
+        <SalesTable />
       </div>
     </>
   );
